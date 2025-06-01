fb8bd8609aec4b0fb76bf80b7c2f8ea25d9144d1
Chhayanka Dewangan <chhayankadewangan2004@gmail.com>
2025-06-01 12:13:51 +0530

    hidden files

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import path from "path"
+
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
@@ -30,6 +32,15 @@ try{
 //definig  routes
 app.use("/book",bookRoute);
 app.use("/user",userRoute);
+
+//deployment
+if(process.env.NODE_ENV==="production"){
+    const dirpath=path.resolve();
+    app.use(express.static("Frontend/dist"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(dirpath,"Frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
